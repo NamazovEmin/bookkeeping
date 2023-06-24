fc481215476aecbfc81a660da60aa46544e79bed
Намазов Эмин Ильгар оглы <ENamazov@cbgr.ru>
2023-06-24 17:36:52 +0300

    Полный парсинг xl,  с возможностью добавить новые банки, новые форматы, Остальное добавить по требованию категории, для сравнения

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,9 +1,10 @@
 
 angular.module('bookkeeping', ['ngStorage']).controller('indexController', function ($scope, $http, $localStorage) {
 
-    $scope.createOperation = function () {
-        // if ($scope.operation.date != null & $scope.operation.type != null & $scope.operation.cost != null &
-        //         $scope.operation.name != null && $scope.operation.category != null) {
+    // $scope.createOperation = function () {
+    //     if ($scope.operation.date != null && $scope.operation.source != null && $scope.operation.cost != null &&
+    //             $scope.operation.name != null && $scope.operation.category != null)
+                {
             $http.post('http://localhost:8080/operation', $scope.operation)
                     .then(function() {
                         $scope.clearPaymentFields();
