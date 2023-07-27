eef08ac8549d6cebf337c57d4fc8add7a3fc2034
Намазов Эмин Ильгар оглы <ENamazov@cbgr.ru>
2023-07-27 20:56:27 +0300

    Make bean JwtTokenFilter and inject them in SecurityConfig

--- src/main/java/az/namazov/bookkeeping/config/security/SecurityConfig.java
+++ src/main/java/az/namazov/bookkeeping/config/security/SecurityConfig.java
@@ -13,27 +13,18 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import az.namazov.bookkeeping.config.security.jwt.JwtTokenFilter;
-import az.namazov.bookkeeping.config.security.jwt.JwtTokenProvider;
-import az.namazov.bookkeeping.config.security.jwt.JwtUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final JwtTokenProvider jwtTokenProvider;
-    private final CustomAuthenticationProvider customAuthenticationProvider;
-    private final JwtUserDetailsService jwtUserDetailsService;
-
+    private final JwtTokenFilter jwtTokenFilter;
     private static final String ADMIN_ENDPOINT = "/admin/**";
     private static final String LOGIN_ENDPOINT = "/v1/login";
 
 
-    public SecurityConfig(@Autowired JwtTokenProvider jwtTokenProvider,
-            @Autowired CustomAuthenticationProvider customAuthenticationProvider,
-            @Autowired JwtUserDetailsService jwtUserDetailsService) {
-        this.jwtTokenProvider = jwtTokenProvider;
-        this.customAuthenticationProvider = customAuthenticationProvider;
-        this.jwtUserDetailsService = jwtUserDetailsService;
+    public SecurityConfig(@Autowired JwtTokenFilter jwtTokenFilter) {
+        this.jwtTokenFilter = jwtTokenFilter;
     }
 
     @Bean
@@ -42,17 +33,11 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.addFilterBefore(new JwtTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
         http.authorizeHttpRequests(auth -> auth
                         .requestMatchers(LOGIN_ENDPOINT).permitAll()
                         .requestMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
                         .anyRequest().authenticated());
-//                .addFilterBefore(new JwtTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers(LOGIN_ENDPOINT).permitAll()
-//                        .requestMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
-//                        .anyRequest().authenticated())
-//                .apply(new JwtConfigurer(jwtTokenProvider));
         return http.build();
     }
 
--- src/main/java/az/namazov/bookkeeping/config/security/jwt/JwtTokenFilter.java
+++ src/main/java/az/namazov/bookkeeping/config/security/jwt/JwtTokenFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
 import jakarta.servlet.FilterChain;
@@ -13,6 +14,7 @@ import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 
+@Component
 @AllArgsConstructor
 public class JwtTokenFilter extends GenericFilterBean {
 
